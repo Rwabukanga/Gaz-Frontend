c9e988a5a9a812a71140aacb3c4cbdad73e75f66
Rwabukanga <43671798+Rwabukanga@users.noreply.github.com>
2024-03-28 16:49:39 +0200

    added datatables

---
+++ src/components/List.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import $ from 'jquery';
+import 'datatables.net';
+
+function Users() {
+    const [reggs, setReggs] = useState([]);
+    const [filterInput] = useState('');
+
+    useEffect(() => {
+        findReggs();
+
+         // Initialize DataTable
+         $('#example').DataTable({
+            language: {
+                emptyTable: "No data available in table",
+                // You can customize other messages here if needed
+            }
+        });
+
+        return () => {
+            // Destroy DataTable instance when component unmounts
+            $('#example').DataTable().destroy();
+        };
+    }, []);
+
+    const findReggs = () => {
+        $('#example').DataTable();
+        axios.get('http://localhost:5001/api/users')
+            .then(response => {
+               
+                console.log(response.data);
+                setReggs(response.data);
+                // Initialize DataTables only after data is fetched
+               
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    };
+
+    return (
+        <div className="card">
+            <h5 className="card-header">Users List</h5>
+            <div className="card-body">
+                <div className="container">
+                    {/* <div className="input-group mb-3">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text"><i className="fas fa-search"></i></span>
+                        </div>
+                        <input className="form-control" type="search" value={filterInput} onChange={e => setFilterInput(e.target.value)} />
+                    </div> */}
+
+                    <table id="example" className="table table-bordered center">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>firstName</th>
+                                <th>lastName</th>
+                                <th>E-mail</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {reggs.filter(regg => regg.firstName.toLowerCase().includes(filterInput.toLowerCase())).map(regg => (
+                                <tr key={regg.id}>
+                                    <td>{regg.id}</td>
+                                    <td>{regg.firstName}</td>
+                                    <td>{regg.lastName}</td>
+                                    <td>{regg.email}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Users;
